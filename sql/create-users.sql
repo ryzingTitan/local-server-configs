676477057edfddcb0ba0561969be66a4a3eca040
Kyle Stoltzfus <computerwhiz33@gmail.com>
2025-12-08 22:57:28 -0500

    Deploy OBDTrak

--- sql/create-users.sql
+++ sql/create-users.sql
@@ -1,2 +1,6 @@
 CREATE USER cashcub WITH PASSWORD 'password';
 GRANT CREATE ON DATABASE cash_cub TO cashcub;
+
+CREATE USER obdtrak WITH PASSWORD 'password';
+GRANT CREATE ON SCHEMA obd_trak TO obdtrak;
+GRANT USAGE ON SCHEMA obd_trak TO obdtrak;
